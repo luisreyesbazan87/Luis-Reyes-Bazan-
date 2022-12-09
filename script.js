734b7c71d6c48ec83a277686e5724d9ce6e5e115
luisreyesbazan87 <120111017+luisreyesbazan87@users.noreply.github.com>
2022-12-08 19:37:55 -0500

    Se agrega el Proyecto

---
+++ script.js
@@ -0,0 +1,7 @@
+
+function saludar(){
+    var nombre;
+    
+    nombre = $("#nombre").val()
+    alert(" mensaje del autor: que le sirva de informacion esta pagina y que tenga un buen dia -> " + nombre)
+}
